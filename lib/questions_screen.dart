46cb108e9a36b5a4ba54948563a7d968a2c53085
Mihir Pratap Sah <smihirp1@gmail.com>
2023-08-20 13:16:31 +0530

    ...

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,21 +12,34 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    currentQuestionIndex++;
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(color: Colors.white),
-        ),
-        const SizedBox(height: 30),
-        ...currentQuestion.answers.map((item) {
-          return AnswerButton(answerText: item, onTap: () {});
-        }),
-      ]),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: const TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              ...currentQuestion.getShuffledAnswers().map((item) {
+                return AnswerButton(answerText: item, onTap: () {});
+              }),
+            ]),
+      ),
     );
   }
 }
